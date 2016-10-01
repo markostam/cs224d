cbe27a564b09ced192d38d403fd1a9288ed59ee9
markostam <markostam@gmail.com>
2016-09-30 21:19:47 -0400

    problem1.1 softmax done

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-def softmax(x):
+def softmax(z):
     """
     Compute the softmax function for each row of the input x.
 
@@ -21,33 +21,37 @@ def softmax(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-    
-    return x
+    min_max_scale = lambda x : (x-np.min(x))/(np.max(x)-np.min(x))
+    row_softmax = lambda x: np.exp(x) / np.sum(np.exp(x))
+    if len(z.shape) == 1:
+        return row_softmax(min_max_scale(z))
+    else:
+    	min_max_scaled = np.apply_along_axis(min_max_scale, 1, z)
+    	softmax_out = np.apply_along_axis(row_softmax, 1, min_max_scaled)
+    	return softmax_out
 
 def test_softmax_basic():
     """
     Some simple tests to get you started. 
     Warning: these are not exhaustive.
     """
-    print "Running basic tests..."
+    print("Running basic tests...")
     test1 = softmax(np.array([1,2]))
-    print test1
+    print(test1)
     assert np.amax(np.fabs(test1 - np.array(
         [0.26894142,  0.73105858]))) <= 1e-6
 
     test2 = softmax(np.array([[1001,1002],[3,4]]))
-    print test2
+    print(test2)
     assert np.amax(np.fabs(test2 - np.array(
         [[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6
 
     test3 = softmax(np.array([[-1001,-1002]]))
-    print test3
+    print(test3)
     assert np.amax(np.fabs(test3 - np.array(
         [0.73105858, 0.26894142]))) <= 1e-6
 
-    print "You should verify these results!\n"
+    print("You should verify these results!\n")
 
 def test_softmax():
     """ 
@@ -56,11 +60,9 @@ def test_softmax():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print "Running your tests..."
+    print("Running your tests...")
     ### YOUR CODE HERE
-    raise NotImplementedError
     ### END YOUR CODE  
 
 if __name__ == "__main__":
     test_softmax_basic()
-    test_softmax()
